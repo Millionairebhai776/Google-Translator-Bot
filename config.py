94eb155cf5268e048d195fdfa4d77398ca49fd91
Millionairebhai776 <105584053+Millionairebhai776@users.noreply.github.com>
2023-02-04 10:23:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 class Config(object):
      
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5880758437:AAERYMlUmGifz6AQIdZkbiHuB6I6XzysR9c")
 
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 2819362))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "578ce3d09fadd539544a327c45b55ee4")
 
     DATABASE = os.environ.get("DATABASE", "")
 
-    DEV_NAME = os.environ.get("DEV_NAME", "Muhammed")
+    DEV_NAME = os.environ.get("DEV_NAME", "Akhil")
 
-    DEV_ID = set(int(x) for x in os.environ.get("DEV_ID", "5601313788").split())
+    DEV_ID = set(int(x) for x in os.environ.get("DEV_ID", "1211202733").split())
